a4516f0346ce67bbe7ecb1b6cbad37dd69f032ce
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-13 18:00:32 -0500

    first commit

---
+++ Release/spivideoplaycontrol.bat
@@ -0,0 +1,2 @@
+rem start spivideoplaycontrol.exe "baraka_chickens(1).avi" -1 100 100 1820 980 220 1 0 0
+start spivideoplaycontrol.exe "baraka_african-ritual.avi" -1 100 100 1820 980 220 1 1 0
---
+++ resource.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2010-2016 Stephane Poirier
+ *
+ * stephane.poirier@oifii.org
+ *
+ * Stephane Poirier
+ * 3532 rue Ste-Famille, #3
+ * Montreal, QC, H2X 2L1
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Windowless.rc
+//
+
+//
+// Resource constants
+//
+#define IDI_MAIN_ICON					100
+#define IDI_MAIN_ICON_SMALL				99
+
+#define IDR_MENU                        101
+#define IDD_ABOUTBOX                    200
+#define ID_FILE_OPENCLIP                40001
+#define ID_FILE_EXIT                    40002
+#define ID_FILE_PAUSE                   40003
+#define ID_FILE_STOP                    40004
+#define ID_FILE_CLOSE                   40005
+#define ID_FILE_MUTE                    40006
+#define ID_FILE_FULLSCREEN              40007
+#define ID_FILE_SIZE_NORMAL             40008
+#define ID_FILE_SIZE_HALF               40009
+#define ID_FILE_SIZE_DOUBLE             40010
+#define ID_FILE_SIZE_QUARTER            40011
+#define ID_FILE_SIZE_THREEQUARTER       40012
+#define ID_HELP_ABOUT                   40014
+#define ID_RATE_INCREASE                40020
+#define ID_RATE_DECREASE                40021
+#define ID_RATE_NORMAL                  40022
+#define ID_RATE_DOUBLE                  40023
+#define ID_RATE_HALF                    40024
+#define ID_SINGLE_STEP                  40025
+#define ID_CAPTURE_IMAGE				40030
+#define ID_DISPLAY_IMAGE				40031
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
